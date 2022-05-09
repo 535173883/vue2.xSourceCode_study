a2f8960414ef70f3267e1131febb7497121f0243
yangtianyu <535173883@qq.com>
2022-05-09 09:34:09 +0800

    vuex实现

--- vue全家桶&原理/router-vuex-study/src/App.vue
+++ vue全家桶&原理/router-vuex-study/src/App.vue
@@ -5,10 +5,21 @@
       <router-link to="/about">About</router-link>
     </div>
     <!-- 占位符 -->
+    <p @click="$store.commit('add')">{{ $store.state.count }}</p>
+    <p @click="click">async{{ $store.state.count }}</p>
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  methods: {
+    click() {
+      console.log(this.$store);
+      this.$store.dispatch("add");
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- vue全家桶&原理/router-vuex-study/src/main.js
+++ vue全家桶&原理/router-vuex-study/src/main.js
@@ -4,12 +4,12 @@ import App from "./App.vue";
 // import router from "./router";
 import router from "./krouter1";
 
-import store from './store'
+import store from "./kstore";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
